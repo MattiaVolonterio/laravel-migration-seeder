87acc7e3a9e0444ad5d6385d3d770ae942fb31ac
MattiaVolonterio <volonteriomattia@gmail.com>
2024-03-20 15:48:09 +0100

    feat: migration create_trains_table

---
+++ database/migrations/2024_03_20_143110_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->enum('company', ['Trenitalia', 'Italo', 'SNCF', 'Deutsche Bahn', 'SBB', 'WestBehn']);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 20);
+            $table->unsignedTinyInteger('number_of_carriages');
+            $table->boolean('in_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
